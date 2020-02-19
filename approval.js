6bbc6cdd4bdfa10645fa2663f68d7a67da15a724
Macon Pegram <mpegram3rd@hotmail.com>
2020-02-19 14:04:58 -0500

    First pass at refactoring code to be more focused/compartmentalized

--- approval.js
+++ approval.js
@@ -1,16 +1,12 @@
-function addEmoji() {
+function approval() {
     var field = document.getElementById("pull_request_review_body");
     if (field) {
         console.log('Found field');
-        field.value = ':+1: ' + getEmoji() + ' ' + field.value;
+        field.value = ':+1: ' + getRandomEmoji() + ' ' + field.value;
         console.log('Set text value to "' + field.value + '"');
     } else {
         console.log('Could not find field');
     }
 }
 
-function getEmoji() {
-    return emojis[Math.floor(Math.random() * emojis.length)];
-}
-
-addEmoji();
+approval();
